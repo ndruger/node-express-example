cb460d3c1ab76dbb0fc18c993e2470bcb992187b
ndruger <snowfullnight@gmail.com>
2014-08-22 00:11:49 +0900

    continuation-local-storage

--- config/deploy.rb
+++ config/deploy.rb
@@ -13,17 +13,10 @@ set :deploy_to, "/var/www/#{fetch(:application)}"
 # Default value for :scm is :git
 set :scm, :git
 
-# Default value for :format is :pretty
-# set :format, :pretty
-
-# Default value for :log_level is :debug
-# set :log_level, :debug
-
-# Default value for :pty is false
-# set :pty, true
-
 # Default value for :linked_files is []
-# set :linked_files, %w{config/database.yml}
+set :linked_files, %w{
+  newrelic.js
+}
 
 # Default value for linked_dirs is []
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
