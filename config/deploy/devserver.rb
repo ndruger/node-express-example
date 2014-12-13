bed7fde3c528922f89ffd0d2a932a083afdc1fad
ndruger <snowfullnight@gmail.com>
2014-12-13 09:59:26 +0900

    deployconfig

--- config/deploy/devserver.rb
+++ config/deploy/devserver.rb
@@ -1,8 +1,18 @@
+#role :app, instances("test").map{ |ip| "snowtest@#{ip}"}
 role :app, instances("test")
 
 set :ssh_options, {
-  user: 'snowtest', # overrides user setting above
+ user: 'snowtest', # overrides user setting above
 #    keys: %w(#{ENV['HOME']}/.ssh/aws_base.pem),
   forward_agent: true,
   auth_methods: %w(publickey)
 }
+
+# server '',
+#   ssh_options: {
+#     user: 'snowtest', # overrides user setting above
+#   #    keys: %w(#{ENV['HOME']}/.ssh/aws_base.pem),
+#     forward_agent: true,
+#     auth_methods: %w(publickey)
+#   }
+
--- script/forever_restart.sh
+++ script/forever_restart.sh
@@ -6,6 +6,7 @@ app=/var/www/node-express-example/current/app.js
 
 source ~/.bash_profile
 cd /var/www/node-express-example/current
-forever stop ${app}
-forever start ${app}
+source /var/www/node-express-example/current/config/config.sh
+NODE_ENV=production forever stop ${app}
+NODE_ENV=production forever start ${app}
 
